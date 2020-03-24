3d2a33e03389359defed48027f9edac95624f51c
jrofurtado <jrofurtado@gmail.com>
2020-03-24 09:15:30 -0100

    .

--- src/index.js
+++ src/index.js
@@ -17,11 +17,11 @@ const history = createBrowserHistory();
 const middleware = [
   thunk,
   routerMiddleware(history),
-];
+]
 const store = createStore(
   rootReducer,
   applyMiddleware(...middleware)
-);
+)
 let kc = null
 if (process.env.NODE_ENV === 'production') {
   kc = new Keycloak('/keycloak.json');
@@ -36,13 +36,25 @@ kc.init({promiseType: 'native', onLoad: 'login-required'}).then(authenticated =>
     console.warn("not authenticated!");
     kc.login();
   }
-});
+})
 axios.interceptors.request.use(config => (
   kc.updateToken(5).then(() => {
     config.headers.Authorization = 'Bearer ' + kc.token;
     return Promise.resolve(config)
   }).catch(kc.login)
-));
+))
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+async function demo() {
+  await sleep(2000)
+  axios.get('/api/apps').then(function (response) {
+    console.log(response.data)
+    console.log(response.status)
+  })
+}
+demo()
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
