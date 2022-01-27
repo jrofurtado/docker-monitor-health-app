f7c9b2191ba9991bcf2fa84010ae0328b788230f
jrofurtado <jrofurtado@gmail.com>
2022-01-27 10:50:04 -0100

    .

--- env.sh
+++ env.sh
@@ -3,6 +3,6 @@
 echo "window._env_ = {" > ./env-config.js
 awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' ./.env >> ./env-config.js
 echo "}" >> ./env-config.js
-echo "window._env_ = {" > ./public/env-config.js
-awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' ./.env >> ./public/env-config.js
-echo "}" >> ./public/env-config.js
+#echo "window._env_ = {" > ./public/env-config.js
+#awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' ./.env >> ./public/env-config.js
+#echo "}" >> ./public/env-config.js
